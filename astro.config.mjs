f3aa94d78556d942ea081ea1ec364fb3281688e2
哈斯 Seth Harding <gearladeldsvon@gmail.com>
2026-01-15 15:40:14 -0500

    Fix astro config and disable jekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 export default defineConfig({
+  // Add your custom domain here
+  site: "https://quantumintelligencesociety.org",
+  // Ensure the base path is root
+  base: "/",
   integrations: [
     tailwind({
       applyBaseStyles: false,
