497f0bfa4c68096711c276d9e8f6495c2ff2c391
哈斯 Seth Harding <gearladeldsvon@gmail.com>
2026-01-15 16:58:58 -0500

    Uodate developed by link

--- src/components/DevelopedBy.tsx
+++ src/components/DevelopedBy.tsx
@@ -14,7 +14,7 @@ const DevelopedBy = () => {
       <span>
         Developed by{" "}
         <a
-          href="https://github.com/austin-hua/NTUQC-Website"
+          href="https://github.com/QuantumIntelligenceSociety/QISWebsite"
           className="inline-block transition duration-300 hover:-translate-y-px outline-none hover:text-primary-10 focus-visible:text-primary-10"
           target="_blank"
           aria-label="Developer"
